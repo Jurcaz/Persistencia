a615d1a90d89884c96a392f2d04bf3835843f5ff
Juan <Juan@DESKTOP-VSNSIHC>
2023-04-25 18:00:34 +0200

    250423 dia trabajo clase 1

--- src/main/java/jpg/iescierva/persistencia/MainClass.java
+++ src/main/java/jpg/iescierva/persistencia/MainClass.java
@@ -27,36 +27,7 @@ public class MainClass {
 		
 //		AlumnoDAO alumnoDAO = new AlumnoMysqlImpl();
 //		Scanner sc = new Scanner(System.in);
-//		int aux;
-//		
-//		System.out.println("Que operacion quiere realizar?");
-//		
-//		do {
-//			aux = 0;
-//			System.out.println("1. Crear usuario, 2. Actualizar usuario, 3. Eliminar usuario, 4. Consultar usuario");
-//			aux = sc.nextInt();
-//			switch (aux) {
-//			case 1:
-//				System.out.println("Introduzca nre");
-//				
-//				break;
-//			case 2:
-//				System.out.println(2);			
-//				break;
-//			case 3:
-//				System.out.println(3);
-//				break;
-//			case 4:
-//				System.out.println(4);
-//				break;
-//			case 5:
-//				System.out.println(5);
-//				break;
-//			default:
-//				break;
-//			}
-//			
-//		} while (aux != 6);
+
 		
 		
 //		AlumnoDTO alumno1 = new AlumnoDTO("12234", "Piqueras", "Pedro", "asdas@ds.cock", "2", "b", "c", new Date(1999, 1, 1), "los ramos", "222222222", "222222222");
